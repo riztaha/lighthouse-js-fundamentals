343e783e7822f38a5f7ef51a1badb797139b80a6
Taha Rizvi <riztaha@gmail.com>
2020-02-21 05:08:45 +0000

    Added While & For loops to print & reverse aan Array.

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0
+while (i < ingredients.length){
+//  console.log(ingredients[i])
+  i++
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (x = 0; x < ingredients.length; x++){
+//  console.log(ingredients[x])
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (z = (ingredients.length - 1); z >= 0; z--){
+  console.log(ingredients[z])
+}
